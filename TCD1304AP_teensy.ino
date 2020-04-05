bd9a16c1138ef1a83e82c5ec704235d4ca4451d8
Nathan Tyrell <ntyrell@Nathans-MBP.T-mobile.com>
2020-04-05 14:33:56 -0400

    added capability to change clock rate; tried framing at 1MHz with 4MHz clock but that didn't quite work..

--- TCD1304AP_teensy.ino
+++ TCD1304AP_teensy.ino
@@ -10,13 +10,18 @@
 #include <ADC.h>
 #include <ADC_util.h>
 
-#define PIXELS 3694 // total number of data samples including dummy outputs
-#define N 10 // number of subdivisions of framerate to expose for (electronic shutter)
+#define PIXELS 3648 // total number of data samples including dummy outputs
+#define N 1 // number of subdivisions of framerate to expose for (electronic shutter)
 #define CLOCK GPIOD_PDOR  // use output of GPIOD on Teensy 3.6
+
+#define ADCS (1<<7) // D7, teensy pin 5, ADC start signal pin
+#define ADCF (1<<0) // D7, teensy pin 2, ADC finish signal pin
 
 #define ICG (1<<4)  // D4 (TCD1304 pin 3, teensy pin 6)
 #define MCLK (1<<2) // D2 (TCD1304 pin 4, teensy pin 7)
 #define SH (1<<3)   // D3 (TCD1304 pin 5, teensy pin 8)  
+
+#define F 2 // clock rate in MHz, 0.5 1 2 or 4 should work..
 
 IntervalTimer frameSampler;
 ADC *adc = new ADC(); // adc object
@@ -26,12 +31,14 @@ void setup(){
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, OUTPUT);
   CLOCK |= ICG; // Set the integration clear gate high.
   // Enable the serial port.
   Serial.begin(115200);
 
   // generate clock frequency of 1MHz on teensy pin 7
-  analogWriteFrequency(7, 1000000);
+  analogWriteFrequency(7, F*1000000);
   analogWrite(7, 124);
 
   // make ADC very fast
@@ -42,7 +49,7 @@ void setup(){
   adc->adc0->startContinuous(A14);   
 
   // to get longer exposure times, set N = 1 and add some extra time 
-  frameSampler.begin(triggerCCD, (float)(7+PIXELS*4) / (float) N);
+  frameSampler.begin(triggerCCD, (float) (6 + (32+PIXELS+14)* 4.0/((float) F)) / (float) N + 0.0);
 }
 
 IntervalTimer CCDsampler;
@@ -57,25 +64,27 @@ void triggerCCD(){
   delayMicroseconds(1); // timing requirement (1000 ns max)
   CLOCK &= ~SH;  // set SH low
   if (t) {
-    delayMicroseconds(5); // timing requirement (min 1000ns, typ 5000 ns)
+    CCDsampler.begin(sampleCCD, 4 / F); // it appears to take about 5us before the first ADC sample happens, but this appears to be related to the 5us delay..
+    delayMicroseconds(4); // timing requirement (min 1000ns, typ 5000 ns); making this the same as (or a multiple of?) the ADC sample period seems to best align the first ADC sample with the rising edge of ICD; adding the delay seems to block the timer from executing
     CLOCK |= ICG;  // set ICG high
-    CCDsampler.begin(sampleCCD, 4); // try calling this here instead of triggering with external interrupt
   }
   c++;
   if (c == N) c = 0; 
 }
 
-uint16_t vals[PIXELS][2];
+uint16_t vals[32+PIXELS][2];
 int i = 0;
 int j = 0;
 int f = 0;
 
 void sampleCCD(){
+  CLOCK ^= ADCS; // toggle ADC start read indicator pin
   // wait for conversion and sample ccd
   while (!adc->adc0->isComplete()); // wait for conversion
   vals[i][j] = (uint16_t)adc->adc0->analogReadContinuous();
+  CLOCK ^= ADCF; // toggle ADC finish read indicator pin; scope measurements show that this reading takes about 0.5us! which means that I could get close to the max clock rate of 4MHz (update: I tried 1MHz sampling of a 10kHz sine wave and it has a few glitches.. might be too much
   i += 1;
-  if (i == PIXELS){
+  if (i == (32+PIXELS)){ // stop sampling before dummy outputs
     CCDsampler.end();
     i = 0;
     // probably trigger a switch to the other array to prevent overwriting, and trigger a send or something
@@ -87,7 +96,7 @@ void sampleCCD(){
 void loop(){
   if (f == 1){
     f = 0;
-    for (int ii = 0; ii < PIXELS; ii++){
+    for (int ii = 32; ii < (32+PIXELS); ii++){
       // could change this to only send the real pixels, not dummies too
       Serial.print(vals[ii][!j]);
       Serial.print(",");
