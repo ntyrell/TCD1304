e7f5486ead6e0b23f51c6a0d77f3c8f517c6e4b2
Nathan Tyrell <ntyrell@Nathans-MBP.T-mobile.com>
2020-04-14 01:23:30 -0400

    updated code for T4. realized that adding delays inside ISR's isn't a great idea. need to fix

--- TCD1304AP_teensy.ino
+++ TCD1304AP_teensy.ino
@@ -1,6 +1,9 @@
 #include <Ethernet3.h>
 #include <EthernetUdp3.h> 
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 /* Original sketch available at:
  * https://hackaday.io/project/18126-dav5-v301-raman-spectrometer/log/53099-using-an-arduino-r3-to-power-the-tcd1304ap-ccd-chip
  * 
@@ -15,19 +18,16 @@
 #include <SPI.h>       
 
 #define PIXELS 3648 // total number of data samples including dummy outputs
-#define N 200 // number of subdivisions of framerate to expose for (electronic shutter)
-#define CLOCK GPIOB_PDOR  // use output of GPIOB on Teensy 3.6
+#define N 50 // number of subdivisions of framerate to expose for (electronic shutter)
+#define CLOCK GPIO9_DR  // use output of GPIOB on Teensy 3.6
 
 #define UDP_TX_PACKET_MAX_SIZE 3*PIXELS/2 // redefine max packet size
 
-#define ADCS (1<<0) // B0, teensy pin 16, ADC start signal pin
-#define ADCF (1<<1) // B1, teensy pin 17, ADC finish signal pin
+#define ICG (1<<4)  // 4 (TCD1304 pin 3, teensy pin 2)
+#define MCLK (1<<5) // 5 (TCD1304 pin 4, teensy pin 3)
+#define SH (1<<6)   // 6 (TCD1304 pin 5, teensy pin 4)  
 
-#define ICG (1<<18)  // B18 (TCD1304 pin 3, teensy pin 29)
-#define MCLK (1<<19) // B19 (TCD1304 pin 4, teensy pin 30)
-#define SH (1<<10)   // B10 (TCD1304 pin 5, teensy pin 31)  
-
-#define F 2// clock rate in MHz, 0.5 1 2 or 4 should work..
+#define F 4// clock rate in MHz, 0.5 1 2 or 4 should work..
 
 IntervalTimer frameSampler;
 IntervalTimer CCDsampler;
@@ -115,32 +115,37 @@ void setup(){
   //Udp.endPacket();
   
   // set clock pins to output
-  pinMode(29, OUTPUT);
-  pinMode(30, OUTPUT);
-  pinMode(31, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   CLOCK |= ICG; // Set the integration clear gate high.
 
   // generate clock frequency of 1MHz on teensy pin 7
-  analogWriteFrequency(30, F*1000000);
-  analogWrite(30, 124);
+  analogWriteFrequency(3, F*1000000);
+  analogWrite(3, 124);
 
   // make ADC very fast
   adc->adc0->setAveraging(1);  
   adc->adc0->setResolution(12);
   adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED);
   adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
-  adc->adc0->startContinuous(A14);   
+  adc->adc0->startContinuous(A0);   
 
   // to get longer exposure times, set N = 1 and add some extra time 
-  CCDsampler.priority(50); // needs high priority to execute more determinisitically?
-  CCDsampler.priority(51); 
+  CCDsampler.priority(0); // needs high priority to execute more determinisitically?
+  frameSampler.priority(1); 
+
+  attachInterrupt(digitalPinToInterrupt(1), beginCCDsampler, RISING);
+  
   frameSampler.begin(triggerCCD, uspf / (float) N + 0.0);
   Serial.println("exiting setup");
 }
 
 int c = 0;
+
+void beginCCDsampler(){
+  CCDsampler.begin(sampleCCD, 4 / F);
+}
 
 void triggerCCD(){
   // this routine sends the message to the CCD to start sending data. CCD will begin when ICG goes high
@@ -154,7 +159,7 @@ void triggerCCD(){
     // it appears to take about 5us before the first ADC sample happens, but this appears to be related to the 5us delay..
     delayMicroseconds(4); // timing requirement (min 1000ns, typ 5000 ns); making this the same as (or a multiple of?) the ADC sample period seems to best align the first ADC sample with the rising edge of ICD; adding the delay seems to block the timer from executing
     CLOCK |= ICG;  // set ICG high
-    CCDsampler.begin(sampleCCD, 4 / F); // with higher priority, this happens at right time..?
+    //CCDsampler.begin(sampleCCD, 4 / F); // with higher priority, this happens at right time..?
   }
   c++;
   if (c == N) c = 0; 
@@ -171,12 +176,10 @@ uint16_t sample = 0;
 uint16_t sample_prev = 0;
 
 void sampleCCD(){
-  CLOCK ^= ADCS; // toggle ADC start read indicator pin
   // wait for conversion and sample ccd
   sample_prev = sample;
   while (!adc->adc0->isComplete()); // wait for conversion
   sample = (uint16_t)adc->adc0->analogReadContinuous();
-  CLOCK ^= ADCF;
   if ((i > 32) && (i%2 == 1)) { // only odd indices
 
     // bit shift and stuff to fit 2 samples into 3 bytes, MSB first
